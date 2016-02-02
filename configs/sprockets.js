01d64f6b56c38ee4c42f26b2aef4d37822ed0881
Sid Wood <sidwood@me.com>
2016-02-02 13:29:05 +0000

    Add sprockets config file

---
+++ configs/sprockets.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  'extends': [
+    'skyscape/configs/core.js'
+  ],
+  'env': {
+    browser: true,
+    jquery: true
+  },
+  'ecmaFeatures': {
+  },
+  'globals': {
+  },
+  'rules': {
+    'strict': [2, 'function'],
+    'spaced-comment': 0
+  }
+};
