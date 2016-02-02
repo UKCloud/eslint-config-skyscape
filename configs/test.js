f3dbfa2e8fa3685d813f8752fa4848c3aa8537a2
Sid Wood <sidwood@me.com>
2016-02-02 13:29:26 +0000

    Add test config file

---
+++ configs/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  'env': {
+    mocha: true,
+    jasmine: true
+  },
+  'rules': {
+    'max-nested-callbacks': 0,
+    'no-magic-numbers': 0,
+    'no-process-env': 0,
+    'no-undefined': 0
+  }
+};
